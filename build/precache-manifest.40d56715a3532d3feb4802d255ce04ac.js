c7f20f0000cebb04765620d62a03c896d76a2468
Logamanickaraja Kandeeswaran <r903952@mc02t22k7h03m.home>
2018-12-31 22:05:08 +0000

    initial commit

---
+++ build/precache-manifest.40d56715a3532d3feb4802d255ce04ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e086b5db8374a83d0fb4",
+    "url": "/static/js/main.e086b5db.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e086b5db8374a83d0fb4",
+    "url": "/static/css/main.982236a7.chunk.css"
+  },
+  {
+    "revision": "bd093c71d4f433c612a14d14c9a1790f",
+    "url": "/index.html"
+  }
+];
